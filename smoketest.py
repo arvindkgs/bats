9f20a204b601b2848fe309ed81d1a4d9692f4d28
akumargs <mail.arvind85@gmail.com>
2019-05-02 13:28:52 +0530

    Forgot to add negative.json

--- smoketest.py
+++ smoketest.py
@@ -1,7 +1,13 @@
 from bin.validation_suite import CompareProperties
+import subprocess
 
 def test_postive_compare():
-    assert CompareProperties("metadatas/tests/positive_test.json").validate() == True
+    passed = CompareProperties("positive.json").validate()
+    process = subprocess.Popen(
+        ['diff metadatas/tests/positive/positive.log metadatas/tests/positive/success.log'], stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE, shell=True)
+    process.communicate()
+    assert passed is True and process.returncode == 0
 
-def test_negative_compare():
-    assert CompareProperties("metadatas/tests/negative_test.json").validate() == False
+def later_test_negative_compare():
+    assert CompareProperties("metadatas/tests/negative_test.json").validate() is False
