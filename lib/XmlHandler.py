3ea227c089ac7d85acb569f616ab43383d4eb22d
akumargs <mail.arvind85@gmail.com>
2019-05-16 15:27:57 +0530

    Using 'libxml2' instead of 'etree' as it does not support full xml-path. Also it's available in dev-pods. Only downside is namespaces. For default ns, I am manually removing it. But for other tags with ns, xpath itself should include it.

--- lib/XmlHandler.py
+++ lib/XmlHandler.py
@@ -1,6 +1,5 @@
-import xml.etree.ElementTree as ET
-from StringIO import StringIO
-
+import libxml2
+import re
 from lib import comparelog
 from Property import Property
 
@@ -11,35 +10,33 @@ def getProperties(resource, extrapolated_properties, files):
         for fileobj in files:
             try:
                 with open(fileobj, 'r') as f:
-                    it = ET.iterparse(StringIO(f.read()))
-                    if it is not None:
-                        for _, el in it:
-                            if '}' in el.tag:
-                                el.tag = el.tag.split('}', 1)[1]  # strip all namespaces
-                        root = it.root
-                        topelement = root.find(".")
-                        if topelement is not None and len(topelement) > 0:
-                            for property in extrapolated_properties:
-                                try:
-                                    split = property.split(".")
-                                    if topelement.tag == split[0]:
-                                        xPath = ''
-                                        for ele in split[1:]:
-                                            ele = ele.replace(' == ', '=')
-                                            ele = ele.replace('==', '=')
-                                            xPath += ele + '/'
-                                        if xPath != '':
-                                            xPath = xPath[:len(xPath) - 1]
-                                            nodes = root.findall(xPath)
-                                            for node in nodes:
-                                                properties.append(Property(property, [node.text]))
-                                except Exception as e:
-                                    comparelog.print_error_log(
-                                        msg="XPath error for property: " + property,
-                                        args={'fnName': resource.testName, 'type': comparelog.MISSING_PROPERTY,
-                                              'source': fileobj, 'checkName': resource.checkName})
+                    doc = libxml2.parseDoc(remove_namespace(f.read()))
+                    root = doc.xpathNewContext()
+                    for property in extrapolated_properties:
+                            try:
+                                nodes = property.split(".")
+                                xPath = '/'
+                                for ele in nodes:
+                                    ele = ele.replace(' == ', '=')
+                                    ele = ele.replace('==', '=')
+                                    xPath += ele + '/'
+                                xPath = xPath[:len(xPath)-1]
+                                if xPath != '':
+                                    # xPath = xPath[:len(xPath) - 1]
+                                    nodes = root.xpathEval(xPath)
+                                    for node in nodes:
+                                        properties.append(Property(property, [node.content]))
+                            except Exception as e:
+                                comparelog.print_error_log(
+                                    msg="XPath error for property: " + property,
+                                    args={'fnName': resource.testName, 'type': comparelog.MISSING_PROPERTY,
+                                          'source': fileobj, 'checkName': resource.checkName})
             except Exception:
                 comparelog.print_error(msg="File not found: " + resource.file,
                                        args={'fnName': resource.testName, 'type': comparelog.FILE_NOT_FOUND,
                                              'source': fileobj, 'checkName': resource.checkName})
     return properties
+
+def remove_namespace(data):
+ data = re.sub('xmlns="[^"]*"\s*', '', data)
+ return data
