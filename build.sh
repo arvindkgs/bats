ab09cc510a2e993e7f598104ff51076ed77cfd90
akumargs <arvind.kumar.gs@oracle.com>
2019-08-12 12:39:08 +0530

    Fixed regression with build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+if [ -f "bats.tar.gz" ]; then rm "bats.tar.gz"; fi;
 if [ -d "dist" ]; then rm -rf dist; fi;
 python setup.py sdist bdist_wheel
-if [ -d "dist" ]; then cp -f dist/basic-acceptance-test-suite-0.0.1.tar.gz dependencies/py_modules; tar czf bats.tar.gz  dependencies -C scripts setup.sh; fi
+if [ -d "dist" ]; then cp -f dist/basic-acceptance-test-suite-0.0.1.tar.gz -t dependencies; cp README.md -t dist/; tar czf bats.tar.gz  dependencies README.md -C scripts setup.sh; fi
